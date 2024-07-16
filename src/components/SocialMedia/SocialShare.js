d849df67581206db500fccb65ae9df6a3d1bf063
egrosdou <egrosdou@cisco.com>
2024-07-16 08:52:26 +0200

    Include the social media icons below the blog posts

---
+++ src/components/SocialMedia/SocialShare.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  LinkedinShareButton,
+  LinkedinIcon,
+  TwitterShareButton,
+  XIcon,
+  RedditShareButton,
+  RedditIcon,
+  EmailShareButton,
+  EmailIcon,
+} from 'react-share';
+
+const SocialShare = ({ title, url }) => {
+  return (
+    <div className="social-share">
+      <LinkedinShareButton url={url}>
+        <LinkedinIcon size={25} round />
+      </LinkedinShareButton>
+      <TwitterShareButton url={url} title={title}>
+        <XIcon size={25} round />
+      </TwitterShareButton>
+      <RedditShareButton url={url} title={title}>
+        <RedditIcon size={25} round />
+      </RedditShareButton>
+      <EmailShareButton url={url} subject={title} body={`Check out this post: ${url}`}>
+        <EmailIcon size={25} round />
+      </EmailShareButton>
+    </div>
+  );
+};
+
+export default SocialShare;
