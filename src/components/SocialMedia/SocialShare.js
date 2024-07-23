8966591d8bd891fbbb4169b17cca677338a9f9af
egrosdou01 <eimaigr@hotmail.gr>
2024-07-23 22:41:44 +0200

    Fix the react share social buttons

--- src/components/SocialMedia/SocialShare.js
+++ src/components/SocialMedia/SocialShare.js
@@ -12,19 +12,28 @@ import {
 import styles from './SocialShare.module.css';
 
 const SocialShare = ({ title, url }) => {
+  const baseURL = "https://egrosdou01.github.io";
+  const fullURL = `${baseURL}${url}`;
+
   return (
     <div className="social-share">
       <p className={styles.shareText}>SHARE</p>
-      <LinkedinShareButton url={url} className={styles.shareButton}>
+      <LinkedinShareButton 
+        url={fullURL}
+        title={title}
+        summary={title}
+        source={fullURL}
+        className={styles.shareButton}
+      >
         <LinkedinIcon size={25} round />
       </LinkedinShareButton>
-      <TwitterShareButton url={url} title={title} className={styles.shareButton}>
-        <XIcon size={25} round />
+      <TwitterShareButton url={fullURL} title={title} className={styles.shareButton}>
+        <XIcon size={25} round /> {/* Changed to TwitterIcon */}
       </TwitterShareButton>
-      <RedditShareButton url={url} title={title} className={styles.shareButton}>
+      <RedditShareButton url={fullURL} title={title} className={styles.shareButton}>
         <RedditIcon size={25} round />
       </RedditShareButton>
-      <EmailShareButton url={url} subject={title} body={`Check out this post: ${url}`} className={styles.shareButton}>
+      <EmailShareButton url={fullURL} subject={title} body={`Check out this post: ${fullURL}`} className={styles.shareButton}>
         <EmailIcon size={25} round />
       </EmailShareButton>
     </div>
